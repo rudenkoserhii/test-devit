1a820e3193126e3a26301cb9cf19ee14d52dd840
rudenkoserhii <rudenko.serhii@ukr.net>
2024-12-02 01:44:36 +0200

    feat: install dotenv

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,9 @@ import { ConfigProvider } from 'antd';
 import 'antd/dist/reset.css';
 import App from 'App';
 import 'index.css';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
   <React.StrictMode>
